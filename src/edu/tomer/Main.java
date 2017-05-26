d2fd7cab450bec495f4c12ea156992fcd84c7995
NessCollege <nesscollege2017@gmail.com>
2017-05-26 11:27:34 +0300

    Maps

--- src/edu/tomer/Main.java
+++ src/edu/tomer/Main.java
@@ -3,20 +3,58 @@ package edu.tomer;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        getNames();
-        Queue<Runnable> runnables = new LinkedList<>();
-        runnables.add(()->{});
+       Map<String, Contact> map = new HashMap<>();
+       map.put("Moe", new Contact("Moe", "green", "050710231"));//android Sharepref, Bundle, Intent(Bundle)
 
 
-        ExecutorService service = Executors.newSingleThreadExecutor();
+        Map<String, Contact> tree = new TreeMap<>(); //
+        tree.put("A", new Contact("Dave", "D", "D"));
+        tree.put("C", new Contact("Gil", "D", "D"));
+        tree.put("B", new Contact("Aba", "D", "D"));
 
-        service.submit(()->{});
+        for (String s : tree.keySet()) {
+            System.out.println(s);
+            System.out.println(tree.get(s));
+        }
+    }
 
+    //Must implement comparable
+    public static void treeSetDemo(){
+        //set Methods:
+        //add, remove
+        Set<String> names = new TreeSet<>();
+        System.out.println(names.add("Codee"));//true
+        System.out.println(names.add("Codee"));//false
+        System.out.println(names.add("Codee"));//false
+        System.out.println(names.add("Mike"));//true
+        System.out.println(names.add("Dave"));//true
+        System.out.println(names.add("Moe")); //true
+        System.out.println(names.add("Moana"));//true
+
+        names.forEach(System.out::println);
+    }
+    public static void hashSetDemo(){
+        //set Methods:
+        //add, remove
+        Set<String> names = new HashSet<>();
+        System.out.println(names.add("Codee"));//true
+        System.out.println(names.add("Codee"));//false
+        System.out.println(names.add("Codee"));//false
+        System.out.println(names.add("Mike"));//true
+        System.out.println(names.add("Dave"));//true
+        System.out.println(names.add("Moe")); //true
+        System.out.println(names.add("Moana"));//true
+        names.forEach(System.out::println);
+
+    }
+    //consumer example:
+    public static void print(String s){
+        System.out.println(s);
     }
 
     public static Queue<String> getNames(){
