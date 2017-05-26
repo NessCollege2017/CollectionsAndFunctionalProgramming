7e0a9d0079a8bce9b45c5de5fcc4ab352b07eab0
NessCollege <nesscollege2017@gmail.com>
2017-05-26 11:35:08 +0300

    Linked List homework

---
+++ src/edu/tomer/MyStringLinkedList.java
@@ -0,0 +1,38 @@
+package edu.tomer;
+
+/**
+ * Created by Android2017 on 5/26/2017.
+ */
+public class MyStringLinkedList {
+    int count;
+    Node head;
+
+    public MyStringLinkedList() {
+        count = 0;
+        head = null;
+    }
+
+    //add(String data)
+    public void add(String data){
+        Node n = new Node(data);
+        if (count == 0){
+            head = n;
+        }else {
+            head.next = n;
+        }
+        count++;
+    }
+
+    public String get(){
+        //
+        return null;
+    }
+
+    class Node{
+        public Node(String data) {
+            this.data = data;
+        }
+        Node next;
+        String data;
+    }
+}
